b34da4bb13809737102d7150939adc891bdc7457
Valeska Leandro <valeskaleandro@gmail.com>
2024-07-03 06:53:36 -0300

    :sparkles: feat: Add search form component.

--- src/app/components/MainHero/MainHero.tsx
+++ src/app/components/MainHero/MainHero.tsx
@@ -7,8 +7,10 @@ import SearchForm from '../SearchForm/SearchForm';
 const MainHero = () => {
   return(
     <div className={styles.mainHero}>
-      <h1 className={styles.welcome}>Welcome.</h1>
-      <h2 className={styles.description}>Millions of movies for you. Explore now.</h2>
+      <div className={styles.containerTexts}>
+        <h1 className={styles.welcome}>Welcome.</h1>
+        <h2 className={styles.description}>Millions of movies for you. Explore now.</h2>
+      </div>
       <SearchForm />
     </div>
   )
--- src/app/components/SearchForm/SearchForm.tsx
+++ src/app/components/SearchForm/SearchForm.tsx
@@ -1,9 +1,40 @@
+'use client'
 // Styles
 import styles from './SearchForm.module.css'
 
+// Icons
+import { IoSearch } from 'react-icons/io5';
+
+// Utils
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 const SearchForm = () => {
+  const router = useRouter()
+  const [search, setSearch] = useState<string>('')
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(search) {
+      const searchFormatted = search.trim().toLowerCase().replace(/ /g, '-')
+      router.push(`/search/${searchFormatted}`)
+      setSearch('')
+    }
+  }
+
   return(
-    <div></div>
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <IoSearch className={styles.icon} size={25}/>
+      <input type="text" 
+      placeholder="Search for movie..." 
+      onChange={(e) => setSearch(e.target.value)}
+      className={styles.inputText}/>
+      
+      <button type="submit" 
+      className={`${styles.buttonSubmit} ${styles.buttonMobile}`}>
+        Search
+      </button>
+    </form>
   )
 }
 
